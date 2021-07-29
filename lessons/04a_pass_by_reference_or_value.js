94a41f0d680652c6dc276c9482071edb1bf8065d
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2021-07-29 18:44:02 +0100

    :truck: rename and move files

--- lessons/04a_pass_by_reference_or_value.js
+++ lessons/04a_pass_by_reference_or_value.js
@@ -52,3 +52,47 @@ console.log(dorianGray);
 // Here, we can see the value of the object hasn't changed, that's because we've created a new reference in memory.
 
 // Read through this —> https://doesitmutate.xyz/
+
+// ## Shallow Copies vs Deep Copies
+
+// In the above updateAgeImmutable function we use the spread operator to copy person
+// But this has two problems:
+// 1. Browser Compatibility - spread operator is part of ES6 JavaScript and not supported in IE or older versions of the other browsers
+// 2. It only makes a shallow copy of an object, any nested objects or arrays are references themselves
+
+// ** Deep Object with Nested Objects
+const josh = {
+    name: "josh",
+    age: 30,
+    occupation: {
+        title: "Senior Analytics Implementation Consultant II",
+        company: "Blast Analytics"
+    }
+}
+
+// When we use the spread syntax, it makes a shallow copy
+// name and occupation are still references
+const shallowJosh = { ... josh };
+
+// When we change a value in a nested object
+shallowJosh.occupation.title = "Analytics Strategist";
+
+// It is also changed in the original
+console.log(josh)
+
+console.log(shallowJosh)
+
+// The easiest way to make a deep copy is to use JSON.parse(JSON.stringify()) syntax
+// First this stringifies the object in full breaking all references
+// Then it parses it back to JSON
+const deepJosh = JSON.parse(JSON.stringify(josh));
+
+// Change a property in a nested object
+deepJosh.occupation.title = "Software Developer";
+
+// And you can see it has only changes in the copy
+console.log(josh);
+
+console.log(deepJosh);
+
+// Additional reading: https://www.freecodecamp.org/news/copying-stuff-in-javascript-how-to-differentiate-between-deep-and-shallow-copies-b6d8c1ef09cd/
